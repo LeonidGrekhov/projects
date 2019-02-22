8edce82803d166108b551f5adf8f3147039b525c
mwinata94 <mwinata@mail.sfsu.edu>
2019-02-22 11:32:20 -0800

    setup AboutPerson page and include an example

--- front-end/src/Containers/Home/Home.js
+++ front-end/src/Containers/Home/Home.js
@@ -1,17 +1,42 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-
+import members from "../About/members";
 class Home extends Component {
   render() {
+    let memberButtons = members.map((memberName, i) => {
+      return (
+        <div key={i}>
+          <br />
+          <Link to={`/about/${memberName}`}>
+            <button type="button" className="btn btn-info">
+              {memberName}
+            </button>
+          </Link>
+          <br />
+        </div>
+      );
+    });
     return (
       <div>
-        <div>Placeholder Home Page</div>
+        <div className="container text-center">
+          <span>Software Engineering class SFSU</span>
+          <br />
+          <span>Spring 2019</span>
+          <br />
+          <span>Section 4</span>
+          <br />
+          <span>Team 4</span>
+          <br />
+        </div>
         <br />
-        <Link to="/about">
-          <button type="button" className="btn btn-info">
-            About Page
-          </button>
-        </Link>
+        <div className="container">
+          <Link to="/about">
+            <button type="button" className="btn btn-info">
+              About Page
+            </button>
+          </Link>
+          {memberButtons}
+        </div>
       </div>
     );
   }
