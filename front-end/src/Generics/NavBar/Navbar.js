b585ac0edea01b4970ad0f7deeed70654e7a1c2e
Jesus Garnica <gtechy100@gmail.com>
2019-04-02 13:53:10 -0700

    Merged with FE-dev
    Added the navbar back in.

--- front-end/src/Generics/NavBar/Navbar.js
+++ front-end/src/Generics/NavBar/Navbar.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import logo from "./logo.svg";
+import "./NavBar.css";
+import cartLogo from "./shoppingCart.svg";
 
 class Navbar extends Component {
+  ComponentWillReceiveProps = props => {};
+
   render() {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    if (this.props.authenticated) {
+      return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <a className="navbar-brand" href="/">
-          Navbar
+          <img src={logo} height="28" alt="banner" />
+          {"  "} ListLister
         </a>
         <button
           className="navbar-toggler"
@@ -21,65 +28,76 @@ class Navbar extends Component {
         </button>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="/">
-                Home <span className="sr-only">(current)</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/">
-                Link
-              </a>
-            </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="/"
-                id="navbarDropdown"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                Dropdown
-              </a>
-              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a className="dropdown-item" href="/">
-                  Action
-                </a>
-                <a className="dropdown-item" href="/">
-                  Another action
-                </a>
-                <div className="dropdown-divider" />
-                <a className="dropdown-item" href="/">
-                  Something else here
-                </a>
-              </div>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link disabled" href="/">
-                Disabled
-              </a>
-            </li>
-          </ul>
+          <ul className="navbar-nav mr-auto" />
           <form className="form-inline my-2 my-lg-0">
-            <input
-              className="form-control mr-sm-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
+          
             <button
-              className="btn btn-outline-success my-2 my-sm-0"
+            variant="primary"
+            id="LoginButton"
+              className="btn btn-outline-primary mr-2 my-2 my-sm-0"
               type="submit"
             >
-              Search
+              Login
             </button>
+            <button variant="primary" id="SignUpButton" className="btn btn-primary my-2 mr-2 my-sm-0" type="submit">
+              Sign Up
+            </button>   
+            <img src={cartLogo} class="mr-2" height="30" width="30" alt="banner" />
+            <button variant="primary" id="cartDiv" className="btn btn-primary my-2 mr-2 my-sm-0" type="submit">
+              {" "}0
+            </button>   
           </form>
+
         </div>
       </nav>
+         
+      );
+    }
+    return  (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a className="navbar-brand" href="/">
+        <img src={logo} height="28" alt="banner" />
+        {"  "} ListLister
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto" />
+        <form className="form-inline my-2 my-lg-0">
+        
+          <button
+          variant="primary"
+          id="LoginButton"
+            className="btn btn-outline-primary mr-2 my-2 my-sm-0"
+            type="submit"
+          >
+            Login
+          </button>
+          <button variant="primary" id="SignUpButton" className="btn btn-primary my-2 mr-2 my-sm-0" type="submit">
+            Sign Up
+          </button>   
+          <img src={cartLogo} class="mr-2" height="30" width="30" alt="banner" />
+          <button variant="primary" id="cartDiv" className="btn btn-primary my-2 mr-2 my-sm-0" type="submit">
+            {" "}0
+          </button>   
+        </form>
+
+      </div>
+    </nav>
+       
     );
+    
+    ;
   }
 }
 export default Navbar;
