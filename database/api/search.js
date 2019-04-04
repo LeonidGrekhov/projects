1744a7246019ac2fc5c4ccda8060af56d2de6471
Vismay15 <vis.patel15@gmail.com>
2019-04-04 00:57:52 -0700

    added support for multi word comparison for search

--- database/api/search.js
+++ database/api/search.js
@@ -1,14 +1,42 @@
+const findBookByTitle = db => title => {
+  var splitString = title.split(" ");
+  console.log(splitString);
 
-const findBookByTitle = db => title =>
-  db.book.findAll(
+  console.log('***************debug:start:')
+  console.log(db.Sequelize.Op.or)
+  
+  let orValue = [];
+  splitString.map( str => {
+    let orValueInteration = {
+      title: {
+        [db.Sequelize.Op.like]: '%' + str + '%'
+      }
+    };
+    orValue.push(orValueInteration);
+  });
+  let where = {
+    [db.Sequelize.Op.or]: orValue
+  }
+  console.log(where);
+  console.log('***************debug:end:')
+  
+  /*
+  return db.book.find(
     { 
       where: { 
         title: {
-          [db.Sequelize.Op.like]: '%' + title + '%'
+          [db.Sequelize.Op.like]: {
+            [db.Sequelize.Op.any]: splitString
+          }
         } 
       }
     }
   );
+  */
+  return db.book.findAll({where});
+};
+   
+
 
 module.exports = db => ({
   findBookByTitle: findBookByTitle(db)
