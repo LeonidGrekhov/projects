e4e850d613f753773703e93a436becdcf10548f2
jmendo12 <jmendo12@mail.sfsu.edu>
2019-04-10 16:21:44 -0700

    Remove debug code from authenticated.js

--- routes/middlewares/authenticated.js
+++ routes/middlewares/authenticated.js
@@ -3,8 +3,6 @@ module.exports = (request, response, next) => {
       response.locals.user = request.user;
       next();
     } else {
-      console.log("*** DEBUG ***\n");
-      console.log("Request is not authenticated");
       response.sendStatus(404);
     }
   };
