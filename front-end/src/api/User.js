8323e1e2352f73243d033ee52fea5aeb2da9a565
Michael <mwinata@mail.sfsu.edu>
2019-04-16 02:30:52 -0700

    jsonify user config api

--- front-end/src/api/User.js
+++ front-end/src/api/User.js
@@ -1,7 +1,8 @@
+import jsonify from './middlewares/jsonify';
 import request from './middlewares/request';
 
 export default {
   getUserProfile: uid => request(`/user/${uid}`, {}, 'get'),
-  getUserConfiguration: uid => request(`/user/${uid}/configuration`, {}, 'get'),
+  getUserConfiguration: uid => request(`/user/${uid}/configuration`, {}, 'get').then(jsonify),
   postUserConfiguration: ({uid, configuration}) => request(`/user/${uid}/configuration`, {configuration})
 };
