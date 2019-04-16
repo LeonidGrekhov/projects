3196790ca6bcb34333b169961f43a856a875892e
Michael <mwinata@mail.sfsu.edu>
2019-04-16 02:11:16 -0700

    include get/post UserConfiguration in User API

--- front-end/src/api/User.js
+++ front-end/src/api/User.js
@@ -1,5 +1,7 @@
 import request from './middlewares/request';
 
 export default {
-  getUserProfile: uid => request(`/user/${uid}`, {}, 'get')
+  getUserProfile: uid => request(`/user/${uid}`, {}, 'get'),
+  getUserConfiguration: uid => request(`/user/${uid}/configuration`, {}, 'get'),
+  postUserConfiguration: ({uid, configuration}) => request(`/user/${uid}/configuration`, {configuration})
 };
