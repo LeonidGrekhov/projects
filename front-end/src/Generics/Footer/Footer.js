5c28c0f012c14aa881a13915b53460cca7c10e13
Michael <mwinata@mail.sfsu.edu>
2019-04-18 18:16:06 -0700

    footer now attached to the footer

--- front-end/src/Generics/Footer/Footer.js
+++ front-end/src/Generics/Footer/Footer.js
@@ -1,32 +1,53 @@
-import React, { Component } from "react";
-import "./Footer.css";
-import { Link } from "react-router-dom";
+import React, { Component } from 'react';
+import './Footer.css';
+import { Link } from 'react-router-dom';
 
 class Footer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      position: 'absolute'
+    };
+  }
+
+  componentDidMount = () => {
+    if (document.body.clientHeight > window.innerHeight) {
+      this.setState({ position: 'relative' });
+    } else {
+      this.setState({ position: 'absolute' });
+    }
+  };
+
   render() {
     const footerLink = [
-      "",
-      "About",
-      "Contact",
-      "Team",
-      "Invest",
-      "Terms of Service"
+      '',
+      'About',
+      'Contact',
+      'Team',
+      'Invest',
+      'Terms of Service'
     ];
     let linkNames = footerLink.map((linkUrl, i) => {
-      var actualLink = linkUrl.split(" ").join("_");
+      var actualLink = linkUrl.split(' ').join('_');
       return (
-        <div className="d-inline  m-2" key={i} >
-          <Link  to={`/${actualLink}`}>
-            <span  className="text-muted">
+        <div className="d-inline  m-2" key={i}>
+          <Link to={`/${actualLink}`}>
+            <span className="text-muted">
               <strong id="footerLink">{linkUrl}</strong>
             </span>
           </Link>
         </div>
       );
     });
-
     return (
-      <div>
+      <div
+        style={{
+          position: this.state.position,
+          right: '0',
+          bottom: '0',
+          left: '0'
+        }}
+      >
         <div
           id="mainAppFooter"
           className="container-fluid p-2 bg-dark justify-content-center"
