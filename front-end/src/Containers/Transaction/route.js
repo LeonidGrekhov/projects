482199ad97674790a20742c2dd123bd0e44ddbb8
Michael <mwinata@mail.sfsu.edu>
2019-04-25 17:29:57 -0700

    finish transaction history

--- front-end/src/Containers/Transaction/route.js
+++ front-end/src/Containers/Transaction/route.js
@@ -1,6 +1,12 @@
 import Transaction from './Transaction';
+import TransactionHistory from './TransactionHistory';
 
 export default [
+  {
+    path: '/transaction/',
+    exact: true,
+    component: TransactionHistory
+  },
   {
     path: '/transaction/:tid',
     exact: true,
