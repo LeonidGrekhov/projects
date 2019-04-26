ee5030d87b6ea313414e8cb793c2b3d24d96a327
Vismay15 <vis.patel15@gmail.com>
2019-04-25 19:39:56 -0700

    added an example for Stripe API call

--- payment/api/charge.js
+++ payment/api/charge.js
@@ -1,3 +1,27 @@
+// Set your secret key: remember to change this to your live secret key in production
+// See your keys here: https://dashboard.stripe.com/account/apikeys
+var stripe = require("stripe")("sk_test_TedK4jxBHA4ZmkT9TNwWopG200Tkbu6zeO");
+
+
+// a sample function to call stripe API and print the response
+async function StripeAPI(){
+  const charge = await stripe.charges.create({
+    amount: 999,
+    currency: 'usd',
+    source: 'tok_visa',
+    receipt_email: 'jenny.rosen@example.com',
+  })
+  console.log('Printing variable after await');
+  console.log(charge);
+}
+
+console.log('Calling Stripe API');
+StripeAPI();
+
+/*
+
+Original Code
+
 const createCharge = stripe => token => {
   console.log('*** DEBUG *** \n');
   console.log('Token value: ' + token + '\n');
@@ -14,3 +38,5 @@ const createCharge = stripe => token => {
 module.exports = stripe => ({
   createCharge: createCharge(stripe)
 });
+
+*/
