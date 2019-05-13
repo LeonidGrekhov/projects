139e0a0c4f24f97da3b594ddcc114b8443a6ecf1
Michael Winata <mwinata@mail.sfsu.edu>
2019-05-12 21:34:58 -0700

    remove debug mode and implement title search

--- front-end/src/api/BookInfo.js
+++ front-end/src/api/BookInfo.js
@@ -2,7 +2,7 @@ import jsonify from './middlewares/jsonify';
 import request from './middlewares/request';
 
 export default {
-  getBookInfo: bid => request(`book/${bid}`, {}, 'get').then(jsonify)
+  getBookInfo: bid => request(`/book/${bid}`, {}, 'get').then(jsonify)
 };
 
 /*
