6bff5cfe4edaed6d48298a12cb37176ae325aba6
emen15 <edwinm.csm1ke@gmail.com>
2019-05-14 16:45:03 -0700

    Adding book recomendations for carousel

--- database/api/book.js
+++ database/api/book.js
@@ -1,4 +1,11 @@
 const getBookData = db => bid => db.book.findByPk(bid);
+
+//Recomendations for carousel returns 5 books from the database
+const getBookDataCarousel = db => () => {
+  return db.book.findAll({ limit: 5 });
+};
+
 module.exports = db => ({
-  getBookData: getBookData(db)
+  getBookData: getBookData(db),
+  getBookDataCarousel: getBookDataCarousel(db)
 });
